070601357a688cfb831b60090c5df35d15534718
juzoace <uzochukwunwigwe@gmail.com>
2023-07-17 11:08:42 +0100

    fixed build issues

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,11 @@
 import { createBrowserRouter } from 'react-router-dom';
-import BlankLayout from '../components/Layouts/BlankLayout';
 import DefaultLayout from '../components/Layouts/DefaultLayout';
 import { routes } from './routes';
 
 const finalRoutes = routes.map((route) => {
     return {
         ...route,
-        element: route.layout === 'blank' ? <BlankLayout>{route.element}</BlankLayout> : <DefaultLayout>{route.element}</DefaultLayout>,
+        element: <DefaultLayout>{route.element}</DefaultLayout>,
     };
 });
 
--- src/store/users/index.ts
+++
@@ -1,5 +0,0 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import userService from './userService';
-
-const user = JSON.parse(localStorage.getItem("users"));
-
